b99617ab68230adee262f7299dcfee7cc131f86a
rapsby <gsu5022@gmail.com>
2020-12-16 16:53:02 +0900

    first commit

---
+++ src/calculator.py
@@ -0,0 +1,29 @@
+class Calculator():
+    
+    def __init__(self):
+        pass
+    
+    @staticmethod
+    def add(arg1, arg2):
+        pass
+
+    @staticmethod
+    def subtract(arg1, arg2):
+        pass
+
+    @staticmethod
+    def multiply(arg1, arg2):
+        pass
+
+    @staticmethod
+    def divide(arg1, arg2):
+        # use //
+        if arg2:
+            pass
+        pass
+
+    @staticmethod
+    def square(arg1):
+        return arg1**2
+
+
---
+++ test/test_calculator.py
@@ -0,0 +1,19 @@
+def test_add(calculator):
+    assert calculator.add(1, 2) == 3
+    assert calculator.add(2, 2) == 4
+
+def test_subtract(calculator):
+    assert calculator.subtract(5, 1) == 4
+    assert calculator.subtract(3, 2) == 1
+
+def test_multiply(calculator):
+    assert calculator.multiply(2, 2) == 4
+    assert calculator.multiply(5, 6) == 30
+    
+def test_divide(calculator):
+    assert calculator.divide(2, 2) == 4
+    assert calculator.divde(5, 6) == 30
+
+def test_square(calculator):
+    assert calculator.square(2) == 4
+    assert calculator.square(5) == 25
